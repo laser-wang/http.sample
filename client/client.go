d28fb3c052f30257e414246fbf0ca28334ab83bf
laser-wang <mnbvc94449@163.com>
2017-02-08 16:17:11 +0800

    qq

--- client/client.go
+++ client/client.go
@@ -45,3 +45,15 @@ func client2() {
 	}
 	fmt.Println(string(body))
 }
+
+func client3() {
+	resp, err := http.Get("http://192.168.0.10:12345/hello")
+	//resp, err := http.Get("http://127.0.0.1:12345/hello")
+	if err != nil {
+		log.Fatal("client1:", err)
+		return
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+
+}
